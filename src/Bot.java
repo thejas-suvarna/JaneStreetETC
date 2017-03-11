f186f2793bed4f23876c99b0f6c9f590a7bd7cb0
Gregory Noma <gregory.noma@gmail.com>
2017-03-11 11:37:09 -0500

    buyBond sellBond

--- src/Bot.java
+++ src/Bot.java
@@ -9,16 +9,40 @@ import java.io.BufferedReader;
 import java.net.Socket;
 
 public class Bot {
+
+    public Bot() {
+
+    }
+
+    public void buyBond() {
+
+    }
+
+    public void sellBond() {
+
+    }
+
     public static void main(String[] args) {
         try
         {
-            Socket skt = new Socket("test-exch-teamname", 20000);
+            Socket skt = new Socket("test-exch-abcde", 20000);
             BufferedReader from_exchange = new BufferedReader(new InputStreamReader(skt.getInputStream()));
             PrintWriter to_exchange = new PrintWriter(skt.getOutputStream(), true);
 
-            to_exchange.println("HELLO TEAMNAME");
+            to_exchange.println("HELLO ABCDE");
             String reply = from_exchange.readLine().trim();
             System.err.printf("The exchange replied: %s\n", reply);
+
+            Bot b = new Bot();
+
+            while(true) {
+                String replyStream = from_exchange.readLine().trim();
+                System.err.printf("The exchange replied: %s\n", replyStream);
+
+                if(replyStream.charAt(5) == 'B') {
+
+                }
+            }
         }
         catch (Exception e)
         {
